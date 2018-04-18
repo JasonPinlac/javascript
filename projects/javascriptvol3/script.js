0b3b624e8c26c43674e41a3518f0ec481f821586
Jason Pinlac <jasonpinlac@gmail.com>
2018-04-17 23:13:06 -0400

    First few projects

---
+++ projects/javascriptvol3/script.js
@@ -0,0 +1,59 @@
+
+// // declaring and creating an object literal
+// var myCoffee = {
+//     // properties
+//     coffeeFlavor : "espresso",
+//     coffeeTemperature : "cold",
+//     coffeeOunces : 100,
+//     coffMilk : true,
+
+//     // methods
+//     reheat : function() {
+//         if(myCoffee.coffeeTemperature === "cold"){
+//             myCoffee.coffeeTemperature = "piping hot!";
+//             alert("Your coffee has been reheated!");
+//         }
+//     }
+// };
+
+// // this is a function.
+// function fook(){
+//     console.log("fook!");
+// }
+
+// // this is an anonymous function assigned to a variable. This is good for passing functions as arguments.
+// var fookYou = function(){
+//     console.log("Fook you!");
+// }
+
+// // Invoking all the functions
+// myCoffee.reheat();
+// fook();
+// fookYou();
+
+//-----------------------------------------------------------------------------------
+
+// var mark = {
+//     name : "Mark",
+//     tShirtColor : "navy blue"
+// };
+
+// var lisa = {
+//     name : "Lisa",
+//     tShirtColor : "red"
+// };
+
+// // object constructors (function to create objects) are used to set up a protoype to generalize the creation of similar objects.
+// function Person(name, tShirtColor){
+//     this.name = name;
+//     this.tShirtColor = tShirtColor;
+// }
+
+
+
+// var jason = new Person("Jason", "purple");
+
+// console.log(jason.name);
+// console.log(jason.tShirtColor);
+
+// ---------------------------------------------------------------------------------
